1daec8906d3f884cecc67c7b2d7f1fbb560f752c
antonio silva <antonio@erpron.com.br>
2025-01-15 18:40:57 -0300

    Instalação do Pacote EntityFramework

---
+++ Infrastructure/Context/AppDbContext .cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Pagamento> Pagamentos { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
--- Infrastructure/Context/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Infrastructure.Context
-{
-    public class Class1
-    {
-
-    }
-}
